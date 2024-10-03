96f4a4b0673ea3aeaff194c9fb55647e71a358de
John Min <john.min@sap.com>
2024-10-03 14:22:44 +0800

    Update build process to clear cache before building

--- build_screensaver.py
+++ build_screensaver.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import subprocess
 import re
-import time
 import tkinter
 import tkinter.filedialog as filedialog
 import tkinter.ttk as ttk
@@ -13,9 +12,14 @@ import cv2
 
 class ScreenSaverBuilder:
     def __init__(self):
-        self.scheme = 'myscreensaver'
-        self.ssbuilder_path = os.path.join(os.path.dirname(__file__), self.scheme)
-        self.asset_path = os.path.join(self.ssbuilder_path, self.scheme)
+        self.xcode_projname = 'myscreensaver'
+        
+        self.root_path = os.path.dirname(__file__)
+        self.ssbuilder_path = os.path.join(self.root_path, self.xcode_projname)
+        self.asset_path = os.path.join(self.ssbuilder_path, self.xcode_projname)
+        self.build_path = os.path.join(self.ssbuilder_path, self.xcode_projname, 'build')
+        self.log_path = os.path.join(self.root_path, 'logs')
+        
         self.ss_path = os.path.join(os.path.expanduser('~'), 'Library', 'Screen Savers')
         
         self.ss_video_name = os.path.join(self.asset_path, 'video')
@@ -32,71 +36,91 @@ class ScreenSaverBuilder:
             cv2.imwrite(self.ss_preview_name, frame)
 
         cap.release()
+    
+    def clear_cache(self):
+        # Clear local build folder
+        if os.path.isdir(self.build_path):
+            shutil.rmtree(self.build_path, ignore_errors=True)
+        
+        # Clear Library DerivedData build folder
+        derived_data = os.path.expanduser('~/Library/Developer/Xcode/DerivedData')
+        if not os.path.isdir(derived_data):
+            return
+        for folder in os.listdir(derived_data):
+            folder_abspath = os.path.join(derived_data, folder)
+            if os.path.isdir(folder_abspath) and folder.startswith(self.xcode_projname):
+                shutil.rmtree(folder_abspath, ignore_errors=True)
+    
+    @staticmethod
+    def refresh_screensaver_procs():
+        # Kill ScreenSaverAgent
+        os.system('killall ScreenSaverAgent')
+        
+        # Kill legacyScreenSaver
+        p = subprocess.Popen(['pgrep', 'legacyScreenSaver'], stdout=subprocess.PIPE)
+        stdout, _ = p.communicate()
+        pids = stdout.decode('utf-8').split()
+        for pid in pids:
+            os.system(f'kill -9 {pid}')
+        
+    def delete_assets(self):
+        assets = [
+            os.path.join(self.asset_path, 'video.mp4'),
+            os.path.join(self.asset_path, 'video.mov'),
+            os.path.join(self.asset_path, 'preview.png')
+        ]
+        for asset in assets:
+            if os.path.exists(asset):
+                os.remove(asset)
+    
+    def install_screensaver(self, ss_name: str):
+        saver_path = os.path.join(self.build_path, f'{ss_name}.saver')
+        shutil.move(saver_path, self.ss_path)
 
     def build_screensaver(self, video_path: str, ss_name: str, set_result: Callable[[str], None]):
         """Build the screensaver using the video_path"""
-        if video_path.endswith(('.mp4', '.mov')):
-            # Read first frame into preview image
-            self.get_preview_image(video_path)
-            
-            # Copy video to asset path
-            video_ext = os.path.splitext(video_path)[1]
-            shutil.copy2(video_path, self.ss_video_name + video_ext)
-            
-            # TODO: try rm -rf ./build and ~/Library/Developer/Xcode/DerivedData/myscreensaver/Build/*
-            
-            xcode_build_cmd = [
-                'xcodebuild',
-                'clean',
-                'build',
-                '-project',
-                os.path.join(self.ssbuilder_path, f"{self.scheme}.xcodeproj"),
-                '-configuration',
-                'Debug',
-                f'PRODUCT_NAME={ss_name}'
-                # '-scheme',
-                # self.scheme,
-                # '-archivePath',
-                # archive_path,
-                # 'archive'
-            ]
-            p = subprocess.Popen(xcode_build_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
-            # TODO: log stderr to tmp file and give user option to open text editor
-            stdout, stderr = p.communicate()
-            with open('build.txt', 'w') as f:
-                f.write(stdout.decode('utf-8'))
-                f.write('\n=======================\n')
-                f.write(stderr.decode('utf-8'))
-                
-            if p.returncode != 0:
-                set_result("Something went wrong!")
-                return
-            
-            # xcode_export_cmd = [
-            #     'xcodebuild',
-            #     '-exportArchive',
-            #     '-archivePath',
-            #     archive_path,
-            #     '-exportPath',
-            #     f'{self.ssbuilder_path}/{ss_name}.saver',
-            #     '-exportOptionsPlist',
-            #     os.path.join(self.ssbuilder_path, 'ExportOptions.plist')
-            # ]
-            # p = subprocess.Popen(xcode_export_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
-            # # TODO: log stderr to tmp file and give user option to open text editor
-            # stdout, stderr = p.communicate()
-            # with open('export.txt', 'w') as f:
-            #     f.write(stdout.decode('utf-8'))
-            #     f.write('\n=======================\n')
-            #     f.write(stderr.decode('utf-8'))
-            # if p.returncode != 0:
-            #     set_result("Something went wrong!")
-            #     return
-            set_result("Screensaver built successfully!")
-        else:
-            set_result("Please select a video file!!")
+        if not video_path.endswith(('.mp4', '.mov')):
+            set_result("Please select a video file in mp4 or mov format")
+            return
+        
+        # Clear previous build cache
+        self.clear_cache()
+        
+        # Read first frame into preview image
+        self.get_preview_image(video_path)
+        
+        # Copy video to asset path
+        video_ext = os.path.splitext(video_path)[1]
+        shutil.copy2(video_path, self.ss_video_name + video_ext)
+        
+        xcode_build_cmd = [
+            'xcodebuild',
+            'clean',
+            'build',
+            '-project',
+            os.path.join(self.ssbuilder_path, f"{self.xcode_projname}.xcodeproj"),
+            '-configuration',
+            'Debug',
+            f'PRODUCT_NAME={ss_name}',
+            f'SYMROOT={self.build_path}',
+            f'CONFIGURATION_BUILD_DIR={self.build_path}'
+        ]
+        p = subprocess.Popen(xcode_build_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        # TODO: log stderr to tmp file and give user option to open text editor
+        stdout, stderr = p.communicate()
+        
+        # TODO: retry once if fail
+        if p.returncode != 0:
+            set_result("Something went wrong")
+            os.system(f'echo "{stderr.decode('utf-8')}" > logs/err.log')
+            return
+        
+        set_result("Screensaver built successfully!")
+        
+        self.install_screensaver(ss_name)
+        self.delete_assets()
+        self.refresh_screensaver_procs()
 
 
 class ScreenSaverUI:
@@ -131,29 +155,37 @@ class ScreenSaverUI:
         ).grid(column=2, row=0, sticky=tkinter.W)
 
         # Result label (don't show yet)
-        result_label = ttk.Label(mainframe, text="", name="result_label")
+        self.result_label = ttk.Label(mainframe, text="", name="result_label")
             
+        # Create a build button
         ttk.Button(
             mainframe,
             text="Build",
-            command=lambda: self.build_screensaver(video_entry.get(), ss_name_entry.get(), result_label)
+            command=lambda: self.build_screensaver(video_entry.get(), ss_name_entry.get())
         ).grid(column=0, row=2, sticky=tkinter.W)
     
-    def build_screensaver(self, video_path: str, ss_name: str, result_label: tkinter.Label):
-        # Create a build button
-        def set_result(res: str):
-            result_label.grid(column=0, row=3, columnspan=3, sticky=tkinter.W)
-            result_label['text'] = res
-        
-        set_result('Building screen saver...')
-        time.sleep(5)
-        self.ssb.build_screensaver(video_path, ss_name, set_result)
+    def set_result(self, res: str):
+        if not res:
+            self.result_label['text'] = res
+            self.result_label.grid_forget()
+        else:
+            self.result_label.grid(column=0, row=3, columnspan=3, sticky=tkinter.W)
+            self.result_label['text'] = res
+        self.result_label.update()
+    
+    def build_screensaver(self, video_path: str, ss_name: str):
+        self.set_result('Building screen saver...')
+        self.ssb.build_screensaver(video_path, ss_name, self.set_result)
 
     def start(self):
         self.root.mainloop()
         
     def browse_for_file(self, video_entry, ss_name_entry):
         """Open a file dialog and fill in the entry with the selected file path"""
+        
+        # Clear result if present
+        self.set_result("")
+        
         video_path = filedialog.askopenfilename(parent=self.root, filetypes=[("Movie files", ".mp4 .mov")])
         if video_path:
             # Update video path
